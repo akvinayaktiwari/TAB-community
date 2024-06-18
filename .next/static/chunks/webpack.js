c87a0e86bf984862f858b26f35b9da2aa7e94af3
Shivang-Zero65 <mr.shivanggupta@gmail.com>
2024-06-18 15:54:08 +0530

    logo and title changed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fb32c7fc5f027b70"; }
+/******/ 		__webpack_require__.h = function() { return "962ee5d562f09702"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
