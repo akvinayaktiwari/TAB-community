f48c0c016e832bcfdec4c8f5ac4b577dbb4b65a4
Shivang-Zero65 <mr.shivanggupta@gmail.com>
2024-06-19 13:02:59 +0530

    update package.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -57,6 +57,11 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	!function() {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -131,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "962ee5d562f09702"; }
+/******/ 		__webpack_require__.h = function() { return "5dbe0428f4a467e5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
