51fb9be7697ef4b8ac718d0ada7dcc7477fb108f
Shivang-Zero65 <mr.shivanggupta@gmail.com>
2024-06-21 11:49:23 +0530

    added next_auth secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,15 +8,20 @@ export default NextAuth({
       clientId:'449756990440-9jsev05a1567fofdkmedi56mci2f5re1.apps.googleusercontent.com',
       clientSecret: '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',
     }),
-  ],
+],
+secret: "DrsyetaCodesWillWork",
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
+        console.log("Sign-in callback:", { user, account, profile });
       return true;
     },
     async redirect({ url, baseUrl }) {
+        console.log("Redirect callback:", { url, baseUrl });
       return baseUrl;
     },
     async session({ session, token, user }) {
+        console.log("Session callback:", { session, user });
+
       return session;
     },
     async jwt({ token, user, account, profile, isNewUser }) {
